95997030117bf6b11e431bdf5e5601fe1cc2b506
PavelDats1 <tiger206135@gmail.com>
2026-05-13 23:02:50 +0300

    lab05 completed: GTest, banking library with 14 tests, mock obje>

---
+++ include/print.hpp
@@ -0,0 +1,11 @@
+#ifndef PRINT_HPP
+#define PRINT_HPP
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+void print(const std::string& text, std::ofstream& out);
+void print(const std::string& text, std::ostream& out = std::cout);
+
+#endif
